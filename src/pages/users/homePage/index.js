e437c368d957e4a9154837cbaf7a99244267e22a
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2025-04-06 11:09:38 +0700

    add description & similar products for product detail page

--- src/pages/users/homePage/index.js
+++ src/pages/users/homePage/index.js
@@ -5,20 +5,14 @@ import cat2Img from "assets/users/images/categories/cat-2.jpg";
 import cat3Img from "assets/users/images/categories/cat-3.jpg";
 import cat4Img from "assets/users/images/categories/cat-4.jpg";
 import cat5Img from "assets/users/images/categories/cat-5.jpg";
-import feature1Img from "assets/users/images/featured/feature-1.jpg";
-import feature2Img from "assets/users/images/featured/feature-2.jpg";
-import feature3Img from "assets/users/images/featured/feature-3.jpg";
-import feature4Img from "assets/users/images/featured/feature-4.jpg";
-import feature5Img from "assets/users/images/featured/feature-5.jpg";
-import feature6Img from "assets/users/images/featured/feature-6.jpg";
-import feature7Img from "assets/users/images/featured/feature-7.jpg";
-import feature8Img from "assets/users/images/featured/feature-8.jpg";
+
 import { ProductCard } from "component";
 import { memo } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "./style.scss";
+import { featProducts } from "utils/common";
 
 const HomePage = () => {
   const responsive = {
@@ -62,109 +56,6 @@ const HomePage = () => {
       name: "Thịt bò",
     },
   ];
-
-  const featProducts = {
-    all: {
-      title: "Toàn bộ",
-      products: [
-        {
-          img: feature1Img,
-          name: "Thịt bò nạt",
-          price: 20000,
-        },
-        {
-          img: feature2Img,
-          name: "Chuối",
-          price: 17800,
-        },
-        {
-          img: feature3Img,
-          name: "Ổi",
-          price: 25000,
-        },
-        {
-          img: feature4Img,
-          name: "Dưa hấu",
-          price: 44020,
-        },
-        {
-          img: feature5Img,
-          name: "Nho tím",
-          price: 120000,
-        },
-        {
-          img: feature6Img,
-          name: "Hamburger",
-          price: 86000,
-        },
-        {
-          img: feature7Img,
-          name: "Xoài keo",
-          price: 69000,
-        },
-        {
-          img: feature8Img,
-          name: "Táo Úc",
-          price: 53000,
-        },
-      ],
-    },
-    freshMeat: {
-      title: "Thịt tươi",
-      products: [
-        {
-          img: feature1Img,
-          name: "Thịt bò nạt",
-          price: 20000,
-        },
-      ],
-    },
-    fruits: {
-      title: "Trái cây",
-      products: [
-        {
-          img: feature2Img,
-          name: "Chuối",
-          price: 17800,
-        },
-        {
-          img: feature3Img,
-          name: "Ổi",
-          price: 25000,
-        },
-        {
-          img: feature4Img,
-          name: "Dưa hấu",
-          price: 44020,
-        },
-        {
-          img: feature5Img,
-          name: "Nho tím",
-          price: 120000,
-        },
-        {
-          img: feature7Img,
-          name: "Xoài keo",
-          price: 69000,
-        },
-        {
-          img: feature8Img,
-          name: "Táo Úc",
-          price: 53000,
-        },
-      ],
-    },
-    fastFood: {
-      title: "Thức ăn nhanh",
-      products: [
-        {
-          img: feature6Img,
-          name: "Hamburger",
-          price: 86000,
-        },
-      ],
-    },
-  };
 
   const renderFeaturedProducts = (data) => {
     const tabList = [];
--- src/pages/users/productDetailPage/index.js
+++ src/pages/users/productDetailPage/index.js
@@ -11,6 +11,8 @@ import {
 import Breadcrumb from "../theme/breadcrumb";
 import "./style.scss";
 import { formatter } from "utils/fomater";
+import { ProductCard } from "component";
+import { featProducts } from "utils/common";
 
 const ProductDetailPage = () => {
   const imgs = [cat1Img, cat2Img, cat3Img];
@@ -58,6 +60,91 @@ const ProductDetailPage = () => {
               </li>
             </ul>
           </div>
+        </div>
+        <div className="product__detail__tab">
+          <h4>Thông tin chi tiết</h4>
+          <div>
+            <ul>
+              <li>
+                <p>
+                  Rau củ chứa rất nhiều vitamin và chất dinh dưỡng nên mang đến
+                  rất nhiều lợi ích cho sức khỏe con người.&nbsp;
+                </p>
+              </li>
+              <li>
+                <p>Hỗ trợ giảm cân</p>
+              </li>
+              <li>
+                <p>
+                  Giảm nguy cơ mắc các bệnh về tim mạch, béo phì và cả ung thư
+                </p>
+              </li>
+              <li>
+                <p>Tăng cường sức đề kháng của cơ thể</p>
+              </li>
+              <li>
+                <p>Cải thiện thị lực</p>
+              </li>
+              <li>
+                <p>Điều hòa đường huyết</p>
+              </li>
+              <li>
+                <p>Giảm cholesterol trong máu</p>
+              </li>
+              <li>
+                <p>Điều hòa huyết áp</p>
+              </li>
+            </ul>
+            <p>
+              <br />
+              <strong>Cách chọn rau củ tươi ngon</strong>
+            </p>
+            <ul>
+              <li>
+                <p>
+                  Dựa vào hình dáng bên ngoài: Nên ưu tiên chọn các loại rau củ
+                  có phần vỏ không có các vết sâu, cuống lá không bị nhũn, thâm
+                  đen. Tránh chọn những loại quả có vẻ ngoài to tròn, căng lớn,
+                  bởi đây có thể là những quả đã bị tiêm thuốc, không an toàn
+                  cho sức khỏe.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Dựa vào màu sắc: Màu sắc của các loại rau củ thường tươi mới,
+                  không có các vết xước, héo hay quá đậm màu. Các loại củ có màu
+                  quá xanh hoặc quá bóng sẽ không hẳn là an toàn cho sức khỏe
+                  người dùng.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Dựa vào mùi hương: Mùi hương tự nhiên của các loại rau củ sẽ
+                  là mùi đặc trưng theo từng loại chứ không phải là mùi hắc,
+                  nồng khó chịu. Nếu các loại củ bạn đang chọn có mùi hóa chất
+                  hãy ngưng sử dụng ngay.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Dựa vào cảm nhận khi cầm: Những loại củ quả cầm chắc tay, kích
+                  thước vừa phải, không quá to sẽ thường ngon hơn những loại to
+                  lớn nhưng khối lượng nhẹ. Một số loại rau củ bạn chỉ nên chọn
+                  những quả nhỏ, đều tay sẽ ngon hơn.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="section-title">
+          <h2>Sản phẩm tương tự</h2>
+        </div>
+        <div className="row">
+          {featProducts.all.products.map((item, key) => (
+            <div key={key} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+              <ProductCard img={item.img} name={item.name} price={item.price} />
+            </div>
+          ))}
         </div>
       </div>
     </>
